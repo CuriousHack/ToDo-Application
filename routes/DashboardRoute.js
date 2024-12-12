8627f7c945e5f62f1dac2b51706aec6a21136527
Akande Lateef <akandelateef0@gmail.com>
2024-12-12 10:56:34 +0100

    add: dashboard routes added

--- routes/DashboardRoute.js
+++ routes/DashboardRoute.js
@@ -1,9 +1,17 @@
 const express = require('express')
+const todoModel = require('../models/TodoModel')
 
 const dashboardRoute = express.Router()
 
-dashboardRoute.get('/', (req, res) => {
-    res.render('dashboard', {user: req.session.user});
+dashboardRoute.get('/', async (req, res) => {
+    try{
+        const todos = await todoModel.find({ user_id: req.session.user.id})
+        res.render('dashboard', {user: req.session.user, todos})
+    }
+    catch(err){
+        res.status(500).render('dashboard', {user: req.session.user, todos: [], message: 'error fetching todo'})
+    }
+    // res.render('dashboard', {user: req.session.user});
 })
 
 module.exports = dashboardRoute
