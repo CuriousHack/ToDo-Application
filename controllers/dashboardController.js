3facb0debe0a46b92b1fa1878bd793d99bf11536
Akande Lateef <akandelateef0@gmail.com>
2024-12-22 16:51:34 +0100

    Add: dashboard segregated

---
+++ controllers/dashboardController.js
@@ -0,0 +1,26 @@
+const { getTodo } = require("../controllers/todoController");
+const todoModel = require('../models/TodoModel')
+
+const dashboardController = async (req, res) => {
+    try{
+        const completedCount = await todoModel.countDocuments({ user_id: req.session.user.id, status: 'completed' });
+        const pendingCount = await todoModel.countDocuments({ user_id: req.session.user.id, status: 'pending' });
+        const totalRelevantTodos = completedCount + pendingCount;
+        let completedPercentage = totalRelevantTodos > 0 
+        ? (completedCount / totalRelevantTodos) * 100 
+        : 0;
+        completedPercentage = Math.round(completedPercentage)
+
+        const todos = await getTodo(req, res)
+        res.render('dashboard', {user: req.session.user, todos, completedPercentage})
+
+    }
+    catch(err){
+        console.log(err)
+        res.status(500).render('dashboard', {user: req.session.user, todos: [], message: 'error fetching todo'})
+    }
+}
+
+module.exports = {
+    dashboardController
+}
